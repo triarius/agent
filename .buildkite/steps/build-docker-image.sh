0090adea8a6fd4fcd08cfa813613388cc747533a
Narthana Epa <narthana.epa@gmail.com>
2023-02-13 23:11:17 +1100

    Remove --name arg

--- .buildkite/steps/build-docker-image.sh
+++ .buildkite/steps/build-docker-image.sh
@@ -57,7 +57,6 @@ if [[ -z "$image_tag" ]] ; then
 fi
 
 builder_name=$(docker buildx create \
-  --name remote \
   --driver remote \
   --driver-opt cacert=/buildkit/certs/ca.pem,cert=/buildkit/certs/cert.pem,key=/buildkit/certs/key.pem \
   tcp://buildkitd.buildkite.svc:1234 \
